f5c034baefdf56127b57f78b9f721fae93f26b2c
Leonardo Nascimento <leonardo.nascimento@cbyk.com.br>
2021-04-21 10:41:35 -0300

    create splash page background gradient

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -1,8 +1,14 @@
+import 'package:DevQuiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: AppGradients.linear),
+      ),
+
+    );
   }
 }
