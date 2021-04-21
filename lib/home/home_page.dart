990e2f7baca56898793564aacf5b84ca2350d622
Leonardo Nascimento <leonardo.nascimento@cbyk.com.br>
2021-04-21 12:02:47 -0300

    Create home page

---
+++ lib/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:DevQuiz/home/widgets/appbar_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
